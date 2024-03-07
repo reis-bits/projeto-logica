55187e89bac0c92e9e615b0d70fc6d86639347c2
Eduardo Reis <eduardorprincepe007@gmail.com>
2024-03-07 18:46:01 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 alert('Boas vindas ao jogo do número secreto.');
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto);
 let tentativas = 1;
 
 let resposta;
 
-while(resposta != numeroSecreto) {
+while (resposta != numeroSecreto) {
     resposta = prompt('Qual o número secreto?');
-    if(resposta == numeroSecreto) {
-        alert(`Você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+    if (resposta == numeroSecreto) {
+        break;
     } else {
-        if(resposta > numeroSecreto) {
+        if (resposta > numeroSecreto) {
             alert(`O número secreto é menor que ${resposta}`);
         } else {
             alert(`O número secreto é maior que ${resposta}`);
@@ -18,3 +18,13 @@ while(resposta != numeroSecreto) {
         tentativas++;
     }
 }
+
+let tentativa = tentativas > 1? 'tentativas' : 'tentativa'
+
+alert(`Você acertou o número secreto ${numeroSecreto} com ${tentativas} ${tentativa}.`);
+
+/*if (tentativas > 1) {
+    alert(`Você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+} else {
+    alert(`Você acertou o número secreto ${numeroSecreto} com ${tentativas} tentativa!`);
+}*/
